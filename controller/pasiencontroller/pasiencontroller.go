e3fda387b5f26aa1ffb1828db39bd9c3eeb2eb67
lulung <alungsatrio12@gmail.com>
2023-06-02 21:53:15 +0700

    create views

--- controller/pasiencontroller/pasiencontroller.go
+++ controller/pasiencontroller/pasiencontroller.go
@@ -2,15 +2,25 @@ package pasiencontroller
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func Index(response http.ResponseWriter, request *http.Request) {
-	fmt.Println("working babi")
+	temp, err := template.ParseFiles("views/pasien/index.html")
+	if err != nil {
+		panic(err)
+	}
+	temp.Execute(response,nil)
 	
 }
 func Add(response http.ResponseWriter, request *http.Request) {
-
+	temp, err := template.ParseFiles("views/pasien/add.html")
+	if err != nil {
+		panic(err)
+	}
+	temp.Execute(response,nil)
+	fmt.Println("halo?")
 }
 func Edit(response http.ResponseWriter, request *http.Request) {
 
